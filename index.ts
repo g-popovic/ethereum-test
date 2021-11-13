8b7d713d1b5ebd13258fd50d3d3be3313125200a
George Popovic <georgepopovich14@gmail.com>
2021-11-13 11:51:25 +0100

    Add account-creation functionality

--- index.ts
+++ index.ts
@@ -5,11 +5,17 @@ config();
 const url = process.env.WEB3_INFURA_URI;
 const web3 = new Web3(url);
 
-async function main() {
+async function fetchAccountBalance() {
 	const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e';
-
 	const balance = await web3.eth.getBalance(address);
 	console.log({ balance: web3.utils.fromWei(balance, 'ether') });
+
+	web3.eth.accounts.create();
 }
 
-main();
+async function createNewAccount() {
+	const account = web3.eth.accounts.create();
+	console.log({ account });
+}
+
+createNewAccount();
